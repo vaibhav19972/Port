31417b8cfb50a0bf35f6095ff06429713457c339
vaibhav19972 <100220382+vaibhav19972@users.noreply.github.com>
2022-04-24 16:54:48 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,92 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+function toggleskills(){
+    let itemclass = this.parentNode.className
+    console.log(this)
+    for( i=0; i<skillsContent.length ; i++  ){
+        skillsContent[i].className = 'skills_content skills_close'
+    }
+    if(itemclass === 'skills_content skills_close')
+        this.parentNode.className = 'skills_content skills_open'
+    
+}
+
+skillsHeader.forEach((el) => {
+    el.addEventListener('click',toggleskills)
+    
+})
+
+/*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContent = document.querySelectorAll('[data-content]')
+tabs.forEach(tab =>{
+      tab.addEventListener('click',() =>{
+          const target = document.querySelector(tab.dataset.target)
+
+          tabContent.forEach(tabcon =>{
+              tabcon.classList.remove('qualification_active')
+          })
+          target.classList.add('qualification_active')
+          
+          tabs.forEach(tab =>{
+              tab.classList.remove('qualification_active')
+          })
+          tab.classList.add('qualification_active')
+      })
+})
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
